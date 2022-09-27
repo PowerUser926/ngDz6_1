e71eb7422aac70ccb256e87cf45508c04b68ec17
Ihor <igorgorbylin@gmail.com>
2022-09-27 17:09:03 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
+import { GoodModel } from './good.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HttpService],
 })
-export class AppComponent {
-  title = 'ngDz6';
+export class AppComponent implements OnInit {
+  goods: GoodModel[] = [];
+
+  constructor(private httpService: HttpService) {}
+
+  ngOnInit() {
+    this.httpService
+      .getGoods()
+      .subscribe((data: any) => (this.goods = data['goodsList']));
+  }
 }
---
+++ src/app/good.model.ts
@@ -0,0 +1,5 @@
+export interface GoodModel {
+  name: string;
+  price: number;
+  image: string;
+}
